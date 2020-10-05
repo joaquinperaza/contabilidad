06aea3db89df4b4d349d4b81d657b1e73aad618c
Joaquin Peraza <joaquin@peraza.uy>
2020-10-05 13:14:35 -0300

    reuse repo

--- firebase-backend/public/js/init.js
+++
@@ -1,9 +0,0 @@
-  var config = {
-    apiKey: "AIzaSyBU4RD1ClAtCgHpRFIac8y1VVyQIeDsLKg",
-    authDomain: "contabilidad-peraza.firebaseapp.com",
-    databaseURL: "https://contabilidad-peraza.firebaseio.com",
-    projectId: "contabilidad-peraza",
-    storageBucket: "contabilidad-peraza.appspot.com",
-    messagingSenderId: "249983775892"
-  };
-  firebase.initializeApp(config);
